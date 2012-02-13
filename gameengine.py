e8e99451b062d5044459f7752c37f12e4431d3f9
Josh <joshp@ubuntu.ubuntu-domain>
2012-02-13 16:58:25 -0500

    A working game now (I said working not fun). Can now move between rooms without getting any kind of errors. Added support for all four directions.

--- gameengine.py
+++ gameengine.py
@@ -2,7 +2,13 @@ class Game(object):
 	"""The Controller for the game. Handle command line input and naviagates the user through the map"""
 	def __init__(self):
 		
-		current_room = MainRoom()
+		main_room = MainRoom()
+		living_room = LivingRoom()
+
+		main_room.set_north(living_room)
+		living_room.set_south(main_room)
+
+		current_room = main_room
 
 		command = None
 
@@ -15,7 +21,7 @@ class Game(object):
 				print "Enter N to go North to the %s" % current_room.north.title
 
 			if current_room.south != None:
-				print "Enter S to go South"
+				print "Enter S to go South to the %s" % current_room.south.title
 			
 			if current_room.east != None:
 				print "Enter E to go East"
@@ -27,6 +33,14 @@ class Game(object):
 
 			if command == "N" and current_room.north != None:
 				current_room = current_room.north
+			elif command == "S" and current_room.south != None:
+				current_room = current_room.south
+			elif command == "E" and current_room.east != None:
+				current_room = current_room.east
+			elif command == "W" and current_room.west != None:
+				current_room = current_room.west
+			elif command == "exit":
+				pass
 			else:
 				print "I'm sorry, but I do not understand that command"
 
@@ -43,10 +57,30 @@ class Room(object):
 	def __init__(self):
 		pass
 
+	def set_north(self, room):
+		self.north = room
+	
+	def set_south(self, room):
+		self.south = room
+	
+	def set_east(self, room):
+		self.east = room
+	
+	def set_west(self, room):
+		self.west = room
+
+	def set_rooms(self, n, e, s, w):
+		set_north(n)
+		set_east(e)
+		set_south(s)
+		set_west(w)
+
 class MainRoom(Room):
 	def __init__(self):
-		self.north = LivingRoom()
+		self.title = "Main Room"
+		self.description = "This is the main room"
 
 class LivingRoom(Room):
 	def __init__(self):
 		self.title = "Living Room"
+		self.description = "A room that appears to have a lot of living in it"
